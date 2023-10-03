7d46c1b8e7a95277f1163eb3b4c978359a8b2b7c
arainwong <arainwong@gmail.com>
2023-10-03 18:49:07 +0200

    change g from uncertainty set to deterministic value

--- DataSampling.m
+++ DataSampling.m
@@ -9,6 +9,7 @@ length = 230/1000; %(m)
 angle_lb = 28/180 * pi;
 angle_ub = 32/180 * pi;
 angleDist = makedist('Uniform','lower',angle_lb,'upper',angle_ub);
+% angle = random(angleDist, 1, 1);
 angle = random(angleDist, 1, numSample);
 
 weight_mean = 25/1000;
@@ -19,7 +20,8 @@ weight = random(weightDist, 1, numSample);
 g_lb = 9.78;
 g_ub = 9.83;
 gDist = makedist('Uniform','lower',g_lb,'upper',g_ub); %m/s^2
-g = random(gDist, 1, numSample);
+g = random(gDist, 1, 1);
+% g = random(gDist, 1, numSample);
 
 friction_mean = 0.5;
 friction_std = 0.1;
