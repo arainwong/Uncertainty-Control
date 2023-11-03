d26f7b071e893575f8469f1c66bd0c67d0789d2b
arainwong <arainwong@gmail.com>
2023-11-03 10:58:31 +0100

    add the configuration of sensitivity analysis in simulink

---
+++ SensitivityAnalysisConfig.m
@@ -0,0 +1,14 @@
+clc; clear;
+
+%% System configuration
+stopTime = 5;
+
+%% Control and optimization
+u = 0;
+
+%% Sensitivity analysis
+length = 230/1000;
+angle = 30/180 * pi;
+weight = 25/1000;
+g = 9.8;
+friction = 0.5;
